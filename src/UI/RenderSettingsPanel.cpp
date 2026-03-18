5aad748fb1d6c07af3c7b643482e9d0abce74d4b
Belle <bellpradier@proton.me>
2026-03-18 10:48:43 +0100

    feature(RenderSettings): Moved shader hot reload to rendersettings panel
    title

--- src/UI/RenderSettingsPanel.cpp
+++ src/UI/RenderSettingsPanel.cpp
@@ -4,6 +4,8 @@
 
 #include "RenderSettingsPanel.h"
 
+#include "Lumiere/ResourcesManager.h"
+
 namespace sun::ui
 {
 RenderSettingsPanel::RenderSettingsPanel(const std::shared_ptr<lum::RendererManager> &renderer)
@@ -17,6 +19,8 @@ void RenderSettingsPanel::Render()
     ImGui::Begin("Render Settings");
     {
         m_pipeline->RenderUI();
+        if (ImGui::Button("Shader Reload"))
+            lum::ResourcesManager::Instance()->ShaderHotReload();
     }
     ImGui::End();
 }
